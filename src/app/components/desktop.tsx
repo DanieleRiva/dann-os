a945452a0e2553426d979381285106342ad1c9f3
DanieleRiva <danieleriva03@gmail.com>
2025-10-25 22:31:29 +0200

    Taskbar toggle fullscreen, added new icons from windows 7, fileSystem.json, load items dynamically from the filesystem, each item has its own cel position and can be dragged around the Desktop

--- src/app/components/desktop.tsx
+++ src/app/components/desktop.tsx
@@ -5,10 +5,21 @@ import { DraggableData, DraggableEvent } from 'react-draggable'
 import GridDisplayer from './gridDisplayer';
 import GridCellHighlighter from './gridCellHighlighter';
 
+interface FileSystemItem {
+    id: number,
+    name: string,
+    icon: string,
+    alt: string,
+    row: number,
+    column: number,
+    componentPath: string
+}
+
 const preferredCellSize = 80;
 
 const Desktop = () => {
-    const [pos, setPos] = useState({ x: 0, y: 0 });
+    const [fileSystem, setFileSystem] = useState<FileSystemItem[]>([]);
+    const [positions, setPositions] = useState<Record<number, { x: number, y: number }>>({});
     const [highlighterPos, setHighlighterPos] = useState<{ x: number, y: number } | null>(null);
     const [grid, setGrid] = useState({ rows: 0, cols: 0, cellWidth: 0, cellHeight: 0 });
     const [isDragging, setIsDragging] = useState(false);
@@ -18,10 +29,20 @@ const Desktop = () => {
     const [showGridDisplayer, setShowGridDisplayer] = useState(false);
     const [showCellHighlighter, setShowCellHighlighter] = useState(true);
 
+
+    // Fetch the fileSystem JSON file
+    useEffect(() => {
+        fetch("/os/fileSystem.json")
+            .then((res) => res.json())
+            .then((data: FileSystemItem[]) => setFileSystem(data))
+            .catch((err) => console.error("Error trying to load the fileSystem: ", err));
+    }, []);
+
+    // Setup the Desktop grid
     useEffect(() => {
         const updateGrid = () => {
             const width = window.innerWidth;
-            const height = window.innerHeight - 64;
+            const height = window.innerHeight - 64; // 64px of taskbar
 
             const cols = Math.floor(width / preferredCellSize);
             const rows = Math.floor(height / preferredCellSize);
@@ -38,7 +59,23 @@ const Desktop = () => {
         return () => window.removeEventListener('resize', updateGrid);
     }, []);
 
-    const onDrag = (e: DraggableEvent, data: DraggableData) => {
+    // Get items positions from the JSON file
+    useEffect(() => {
+        if (grid.cellWidth === 0 || grid.cellHeight === 0 || fileSystem.length === 0) return;
+
+        const fileSystemPositions = fileSystem.reduce((accumulator: any, item) => {
+            accumulator[item.id] = {
+                x: item.column * grid.cellWidth,
+                y: item.row * grid.cellHeight
+            };
+
+            return accumulator;
+        }, {} as Record<number, { x: number, y: number }>);
+
+        setPositions(fileSystemPositions);
+    }, [grid, fileSystem]);
+
+    const onDrag = (itemId: number, e: DraggableEvent, data: DraggableData) => {
         if ('clientX' in e) {
             setIsDragging(true);
 
@@ -52,19 +89,31 @@ const Desktop = () => {
         }
     }
 
-    const onDragStop = (e: DraggableEvent, data: DraggableData) => {
+    const onDragStop = (itemId: number, e: DraggableEvent, data: DraggableData) => {
         if ('clientX' in e) {
-            setIsDragging(false);
-
             const pointerX = e.clientX;
             const pointerY = e.clientY;
 
             const snappedX = Math.floor(pointerX / grid.cellWidth) * grid.cellWidth;
             const snappedY = Math.floor(pointerY / grid.cellHeight) * grid.cellHeight;
 
-            setPos({ x: snappedX, y: snappedY });
+            // Iterate 
+            for (const [, position] of Object.entries(positions)) {
+                if (position.x === snappedX && position.y === snappedY) {
+                    setIsDragging(false);
+                    setHighlighterPos(null);
+                    return;
+                }
+            }
+
+            // Copy all of the previous positions and update the dragged item's
+            setPositions(prevState => ({
+                ...prevState,
+                [itemId]: { x: snappedX, y: snappedY }
+            }));
         }
 
+        setIsDragging(false);
         setHighlighterPos(null);
     }
 
@@ -73,16 +122,56 @@ const Desktop = () => {
         console.log(e);
     }
 
+    if (Object.keys(positions).length === 0)
+        return (
+            <main className='relative w-screen h-[calc(100vh-64px)] bg-cover bg-no-repeat bg-[image:var(--desktop-wallpaper)] flex justify-center items-center overflow-hidden'>
+                <span className='bg-gray-700 text-white rounded-lg p-4'>
+                    Loading DANNos...
+                </span>
+            </main>
+        );
+
     return (
-        <main className='relative w-screen h-[calc(100vh-64px)] bg-cover bg-no-repeat bg-[image:var(--desktop-wallpaper)] p-0 overflow-hidden'>
+        <main className='relative w-screen h-[calc(100vh-64px)] bg-cover bg-no-repeat bg-[image:var(--desktop-wallpaper)] overflow-hidden'>
 
             <GridDisplayer active={showGridDisplayer} grid={grid} />
 
             <GridCellHighlighter active={showCellHighlighter} grid={grid} highlighterPos={highlighterPos} highlighterColor='bg-gray-700' />
 
-            <Rnd
+            {fileSystem.map(item => (
+                <Rnd
+                    key={item.id}
+                    size={{ width: grid.cellWidth, height: grid.cellHeight }}
+                    position={positions[item.id]}
+                    onDoubleClick={onDoubleClick}
+                    onDrag={(e, data) => onDrag(item.id, e, data)}
+                    onDragStop={(e, data) => onDragStop(item.id, e, data)}
+                    bounds="parent"
+                    enableResizing={false}
+                    className='cursor-pointer z-10 p-2'
+                >
+                    <div
+                        className={`w-full h-full flex flex-col items-center justify-evenly text-center rounded-lg select-none ${isDragging ? "cursor-grabbing" : "cursor-pointer"}`}>
+                        <img
+                            src={item.icon || "/icons/default.svg"}
+                            width={grid.cellHeight * cellIconRatio}
+                            className='pointer-events-none select-none mx-auto aspect-square'
+                            alt="Icona cartella"
+                        />
+
+                        <div
+                            className="flex items-center justify-center w-full"
+                            style={{ height: grid.cellHeight * (1 - cellIconRatio) }}
+                        >
+                            <span className="select-none text-center">{item.name}</span>
+                        </div>
+                    </div>
+                </Rnd>
+            ))}
+
+            {/* <Rnd
                 size={{ width: grid.cellWidth, height: grid.cellHeight }}
-                position={pos}
+                position={positions}
                 onDoubleClick={onDoubleClick}
                 onDrag={onDrag}
                 onDragStop={onDragStop}
@@ -106,7 +195,7 @@ const Desktop = () => {
                         <span className="select-none text-center">Notepad</span>
                     </div>
                 </div>
-            </Rnd>
+            </Rnd> */}
 
         </main>
     );
--- src/app/components/taskbar.tsx
+++ src/app/components/taskbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client"
+
+import { useState } from "react";
 
 const Taskbar = () => {
+    const [fullscreen, setFullscreen] = useState(false);
+
+    const toggleFullscreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+            setFullscreen(true);
+        } else {
+            document.exitFullscreen();
+            setFullscreen(false);
+        }
+    }
+
     return (
-        <footer className='w-full h-16 bg-neutral-800 flex justify-center items-center'>
+        <footer className='w-full h-16 bg-neutral-800 flex justify-between items-center px-4'>
+            <button>Start</button>
+
             taskbar
+
+            <button className="cursor-pointer" onClick={toggleFullscreen}>⛶</button>
         </footer>
     )
 }
