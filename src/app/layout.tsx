45f24c7f9f48193a395c9fb440d838003f202ff2
DanieleRiva <danieleriva03@gmail.com>
2025-10-28 18:04:13 +0100

    Vercel deployment fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// @ts-ignore
 import "./globals.css";
 import Desktop from "./components/desktop";
 import Taskbar from "./components/taskbar";
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    // Vercel deployment fix. Website content is handled in the layout.tsx
+    return null;
+}
+
+export default Page
